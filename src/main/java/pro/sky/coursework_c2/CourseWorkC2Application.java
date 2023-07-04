40dac61e93ebc37ea3bfc8d32bc6cd2792f731c5
MariaLanss <maruska0990@gmail.com>
2023-07-04 15:05:47 +0300

    .

---
+++ src/main/java/pro/sky/coursework_c2/CourseWorkC2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework_c2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkC2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkC2Application.class, args);
+    }
+
+}
